b143bb5112b70567f55234514ecd49c97b1387cf
Ankit Kadere <ankitkadere2904@gmail.com>
2024-11-06 21:58:33 +0530

    Add files via upload

---
+++ towMaxNum.java
@@ -0,0 +1,27 @@
+package LMS;
+import java.util.Scanner;
+public class towMaxNum {
+ 
+	public static void main(String[] args) {
+	    Scanner myObj = new Scanner(System.in);
+	    
+	    System.out.println("Enter the Number"); 
+	  int n1 = myObj.nextInt();
+	  
+	  	    
+	    System.out.println("Enter the Number-2"); 
+	  int n2 = myObj.nextInt();
+                
+                if(n1>n2){
+                       System.out.println(" Number:1 is grater than number-2"); 
+                }
+                else if(n2>n1){
+                    System.out.println(" Number:2 is grater than number-1"); 
+                }
+                else{
+                     System.out.println(" Both numbers are equal"); 
+                }
+                
+	}  
+}
+
